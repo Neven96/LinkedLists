0d48f42089c3bee5b01dedbe7f491dc4c2b47ede
Even <even.holmen@hotmail.no>
2023-12-09 12:24:57 +0100

    Updated Rust LinkedList

--- Rust/linked_list/src/list/linked_lists.rs
+++ Rust/linked_list/src/list/linked_lists.rs
@@ -9,7 +9,7 @@ pub struct LinkedList<T: std::clone::Clone> {
     pub length: i32,
 }
 
-impl<T: std::clone::Clone> LinkedList<T> {
+impl<T: std::clone::Clone + std::fmt::Debug> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList {
             head: None,
@@ -21,6 +21,8 @@ impl<T: std::clone::Clone> LinkedList<T> {
     //
     //
     // INSERT NODES
+    // insert_first and insert_last does not "communicate" with eachother, 
+    // so any insert_last does not become the next node for insert_first...
     //
     //
     pub fn insert_first(&mut self, data: T) {
@@ -63,9 +65,9 @@ impl<T: std::clone::Clone> LinkedList<T> {
     //
     //
     pub fn get_first(&mut self) -> Option<T> {
-        match self.head.take() {
+        match &self.head {
             Some(head_value) => {
-                return Some(head_value.value);
+                return Some(head_value.value.clone());
             }
             None => {
                 return None;
@@ -74,13 +76,44 @@ impl<T: std::clone::Clone> LinkedList<T> {
     }
 
     pub fn get_last(&mut self) -> Option<T> {
-        match self.tail.take() {
+        match &self.tail {
             Some(tail_value) => {
-                return Some(tail_value.value);
+                return Some(tail_value.value.clone());
             }
             None => {
                 return None;
             }
         }
+    }
+
+    pub fn get_list(&mut self) -> Vec<T> {
+        let mut list_array = Vec::new();
+
+        if self.length == 0 {
+            panic!("List is empty")
+        }
+
+        if self.length > 0 {
+            let mut current_node = self.get_actual_node(self.head.clone());
+            list_array.push(current_node.value.clone());
+            
+            while current_node.next.is_some() {
+                current_node = self.get_actual_node(current_node.next.clone());
+                list_array.push(current_node.value.clone());
+            }
+            
+        }
+
+        return list_array;
+    }
+
+    //
+    //
+    // "HELPER" FUNCTIONS
+    //
+    //
+
+    pub fn get_actual_node(&mut self, node: Option<Box<Node<T>>>) -> Node<T> {
+        return Box::into_inner(<Option<Box<Node<T>>> as Clone>::clone(&node).expect("No Node"));
     }
 }
--- Rust/linked_list/src/main.rs
+++ Rust/linked_list/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(box_into_inner)]
 use crate::list::linked_lists;
 use crate::linked_lists::LinkedList;
 use crate::list::node;
@@ -7,9 +8,11 @@ pub mod list;
 fn main() {
     let mut linked_list: linked_lists::LinkedList<i32> = LinkedList::new();
     linked_list.insert_first(5);
-    linked_list.insert_last(7);
+    linked_list.insert_first(7);
     linked_list.insert_first(6);
-    println!("{:?}", linked_list);
-    println!("{:?}", linked_list.get_first());
-    println!("{:?}", linked_list.get_last());
+    // println!("{:?}", linked_list);
+    // println!("{:?}", linked_list.get_first());
+    // println!("{:?}", linked_list.get_first());
+    // println!("{:?}", linked_list.get_last());
+    println!("{:?}", linked_list.get_list());
 }
