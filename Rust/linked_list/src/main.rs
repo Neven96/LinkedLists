ce99e6fc6b30510efd03d1072e253d99cd4ab580
Even <even.holmen@hotmail.no>
2024-01-01 17:42:16 +0100

    Updated Rust LinkedList

--- Rust/linked_list/src/main.rs
+++ Rust/linked_list/src/main.rs
@@ -2,17 +2,35 @@
 use crate::list::linked_lists;
 use crate::linked_lists::LinkedList;
 use crate::list::node;
-
 pub mod list;
 
 fn main() {
-    let mut linked_list: linked_lists::LinkedList<i32> = LinkedList::new();
-    linked_list.insert_first(5);
-    linked_list.insert_first(7);
-    linked_list.insert_first(6);
-    // println!("{:?}", linked_list);
-    // println!("{:?}", linked_list.get_first());
-    // println!("{:?}", linked_list.get_first());
-    // println!("{:?}", linked_list.get_last());
-    println!("{:?}", linked_list.get_list());
+    // let mut linked_list: linked_lists::LinkedList<i32> = LinkedList::new();
+    // linked_list.insert_first(1);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::list::linked_lists;
+    use crate::linked_lists::LinkedList;
+
+    // This works
+    #[test]
+    fn insert_first_tests() {
+        let mut linked_list: linked_lists::LinkedList<i32> = LinkedList::new();
+        linked_list.insert_first(5);
+        linked_list.insert_first(7);
+        linked_list.insert_first(6);
+        assert_eq!(linked_list.get_list(), vec![6, 7, 5]);
+    }
+
+    // This does not work...
+    #[test]
+    fn insert_last_test() {
+        let mut linked_list: linked_lists::LinkedList<i32> = LinkedList::new();
+        linked_list.insert_last(5);
+        linked_list.insert_last(7);
+        linked_list.insert_last(6);
+        assert_eq!(linked_list.get_list(), vec![5, 7, 6]);
+    }
 }
